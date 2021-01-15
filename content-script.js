a1423ccd9a2cba4110ce811855977f6811193e07
Balazs Danyadi <balazs.danyadi@gmail.com>
2021-01-15 13:52:17 +0100

    warning message style

--- content-script.js
+++ content-script.js
@@ -5,24 +5,29 @@ testAd.innerHtml = "&nbsp";
 testAd.className = "adsbox";
 document.body.appendChild(testAd);
 
-window.setTimeout(function () {
-  if (testAd.offsetHeight === 0) {
-    document.body.textContentContent = "";
+// main extension logic
+// checking if ads can be shown or  not
+const AdBlockChecker = () =>
+  window.setTimeout(function () {
+    if (testAd.offsetHeight === 0) {
+      document.body.textContent = "";
 
-    let header = document.createElement("div");
-    let headerTitle = document.createElement("h1");
-    let headerText = document.createElement("h3");
+      let header = document.createElement("div");
+      let headerTitle = document.createElement("h1");
+      let headerText = document.createElement("h3");
 
-    header.classList.add("headerCont");
-    headerTitle.classList.add("headerTitle");
-    headerText.classList.add("headerText");
+      header.classList.add("headerCont");
+      headerTitle.classList.add("headerTitle");
+      headerText.classList.add("headerText");
 
-    headerTitle.textContent("AdBlocker is active!");
-    headerText.textContent("Please deactivate and refresh to continue.");
+      headerTitle.textContent = "AdBlocker is active!";
+      headerText.textContent = "Please deactivate and refresh to continue.";
 
-    document.body.appendChild(header);
-    header.appendChild(headerTitle);
-    header.appendChild(headerText);
-  }
-  testAd.remove();
-}, 100);
+      document.body.appendChild(header);
+      header.appendChild(headerTitle);
+      header.appendChild(headerText);
+    }
+    testAd.remove();
+  }, 100);
+
+AdBlockChecker();
