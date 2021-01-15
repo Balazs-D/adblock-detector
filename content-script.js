45372d53c3c63726d922be69340bf6c11177e064
Balazs Danyadi <balazs.danyadi@gmail.com>
2021-01-15 13:20:00 +0100

    adblock detector is active on refresh and new tab

--- content-script.js
+++ content-script.js
@@ -6,20 +6,23 @@ testAd.className = "adsbox";
 document.body.appendChild(testAd);
 
 window.setTimeout(function () {
-  document.body.textContentContent = "";
+  if (testAd.offsetHeight === 0) {
+    document.body.textContentContent = "";
 
-  let header = document.createElement("div");
-  let headerTitle = document.createElement("h1");
-  let headerText = document.createElement("h3");
+    let header = document.createElement("div");
+    let headerTitle = document.createElement("h1");
+    let headerText = document.createElement("h3");
 
-  header.classList.add("headerCont");
-  headerTitle.classList.add("headerTitle");
-  headerText.classList.add("headerText");
+    header.classList.add("headerCont");
+    headerTitle.classList.add("headerTitle");
+    headerText.classList.add("headerText");
 
-  headerTitle.textContent("AdBlcoker is active!");
-  headerText.textContent("Please deactivate and refresh to continue.");
+    headerTitle.textContent("AdBlocker is active!");
+    headerText.textContent("Please deactivate and refresh to continue.");
 
-  document.body.appendChild(header);
-  header.appendChild(headerTitle);
-  header.appendChild(headerText);
-});
+    document.body.appendChild(header);
+    header.appendChild(headerTitle);
+    header.appendChild(headerText);
+  }
+  testAd.remove();
+}, 100);
