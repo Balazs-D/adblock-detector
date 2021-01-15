f2dc20da5f00b672a2f9e39dfe5776fd68598ebb
Balazs Danyadi <balazs.danyadi@gmail.com>
2021-01-15 14:49:47 +0100

    server log incl. time and date stamp works

--- content-script.js
+++ content-script.js
@@ -4,39 +4,37 @@ testAd.innerHTML = "&nbsp;";
 testAd.className = "adsbox";
 document.body.appendChild(testAd);
 
-const AdBlockChecker = () =>
-  window.setTimeout(function () {
-    if (testAd.offsetHeight === 0) {
-      postLog(true);
-      document.body.textContent = "";
+window.setTimeout(function () {
+  if (testAd.offsetHeight === 0) {
+    postLog(true);
+    document.body.textContent = "";
 
-      let header = document.createElement("div");
-      let headerTitle = document.createElement("h1");
-      let headerText = document.createElement("h3");
+    let header = document.createElement("div");
+    let headerTitle = document.createElement("h1");
+    let headerText = document.createElement("h3");
 
-      header.classList.add("headerCont");
-      headerTitle.classList.add("headerTitle");
-      headerText.classList.add("headerText");
+    header.classList.add("headerCont");
+    headerTitle.classList.add("headerTitle");
+    headerText.classList.add("headerText");
 
-      headerTitle.textContent = "AdBlocker is active!";
-      headerText.textContent = "Please deactivate and refresh to continue.";
+    headerTitle.textContent = "AdBlocker is active!";
+    headerText.textContent = "Please deactivate and refresh to continue.";
 
-      document.body.appendChild(header);
-      header.appendChild(headerTitle);
-      header.appendChild(headerText);
-    }
-    postLog(adBlockEnabled);
-    testAd.remove();
-  }, 100);
+    document.body.appendChild(header);
+    header.appendChild(headerTitle);
+    header.appendChild(headerText);
+  }
+  postLog(adBlockEnabled);
+  testAd.remove();
+}, 100);
 
 const postLog = (isBlocked) => {
-  let dateStamp = ([month, date, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/"));
-  let timeStamp = ([hour, minute, second] = new Date()
-    .toLocaleTimeString("en-US")
-    .split(/:| /));
-  let timeDateStamp = timeStamp + dateStamp;
+  d = new Date();
+  let hour = d.getHours();
+  let min = d.getMinutes();
+  let s = d.getSeconds();
+  let timeDateStamp = hour + ":" + min + ":" + s + " / " + d.toDateString();
+
   let blocked = isBlocked;
 
   fetch("http://localhost:3000/logs/", {
